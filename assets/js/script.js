8d0f09c68662b9b1611628c291c6f1586c40aebb
maideas <maik.heese@web.de>
2020-12-01 00:00:24 +0100

    some re-styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,6 @@
 
 $(document).ready(function() {
 
-  /* make image paragraph styling possible */
-  $("img").each(function() {
-    var par = $(this).parent()
-    var parpar = $(this).parent().parent()
-    if (par.is("p")) {
-      //par.before('<div class="clearfix"></div>')
-      par.addClass('img')
-    }
-    if (par.is("a")) {
-      if (parpar.is("p")) {
-        //parpar.before('<div class="clearfix"></div>')
-        parpar.addClass('img')
-      }
-    }
-  })
-
   /* add top-ul for CSS styling and "span" wrap top li texts */
   $("#sidebar-box ul").each(function() {
     if (!$(this).parent().is("li")) {
@@ -32,6 +16,7 @@ $(document).ready(function() {
   })
 
   $("#header i.fas").on('click', () => {
+    $(window).scrollTop(0)
     $('#sidebar').toggleClass('show-menu')
     $('#content').toggleClass('show-menu')
     $("#header i.fas").toggleClass('show-icon')
@@ -39,6 +24,7 @@ $(document).ready(function() {
 
   $("a.show-nav-bar").on('click', () => {
     if ($(window).width() < 900) {
+      $(window).scrollTop(0)
       $('#sidebar').addClass('show-menu')
       $('#content').addClass('show-menu')
       $("#header i.fa-bars").removeClass('show-icon')
